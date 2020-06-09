b0af61edd19c0fe23947a7c0abf6212c481e11f3
sara altaee <sara.korea96@gmail.com>
2020-06-09 16:25:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array,string){
+ return array.filter(item => item === string.toLowerCase() || item === string.toUpperCase() || item === string.charAt(0).toUpperCase() + string.slice(1))
+}
+
+
+function fuzzyMatch(array ,string){
+  return array.filter(item=>item.startsWith(string))
+}
+
+function matchName(array , string){
+  return array.filter(item=>item.name===string)
+}
