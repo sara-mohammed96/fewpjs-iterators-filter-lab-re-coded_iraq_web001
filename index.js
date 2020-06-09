b517622eedc700318cfe5b9856cbec4213c05734
sara altaee <sara.korea96@gmail.com>
2020-06-09 16:15:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 function findMatching(array,string){
- return array.filter(item => item === string.toLowerCase() || item === string.toUpperCase() || item === string.charAt(0).toUpperCase() || item===string.charAt(0).toLowerCase+ string.slice(1))
+ return array.filter(item => item === string.toLowerCase() || item === string.toUpperCase() || item === string.charAt(0).toUpperCase() || item===string.charAt(0).toLowerCase()+ string.slice(1))
 }
