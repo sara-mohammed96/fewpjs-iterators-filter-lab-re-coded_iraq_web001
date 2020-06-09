1de20dd5de1a23c6f652be1529b942dcb8402fce
sara altaee <sara.korea96@gmail.com>
2020-06-09 16:13:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution here
 function findMatching(array,string){
- let variable= array.filter(item => item === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
-return variable;
+ return array.filter(item => item === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
 }
