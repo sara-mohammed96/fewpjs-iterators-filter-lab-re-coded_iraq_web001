fc408863a12fc92edaac60bf2f3a2844bb956c21
sara altaee <sara.korea96@gmail.com>
2020-06-09 16:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function findMatching(array,string){
  return array.filter(item => item === string.toLowerCase() || item === string.toUpperCase() || item === string.charAt(0).toUpperCase() + string.slice(1))
 }
+
+
+function fuzzyMatch(array ,string){
+  return array.filter(item=>item ===)
+}
