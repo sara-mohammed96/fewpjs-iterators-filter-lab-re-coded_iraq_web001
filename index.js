fce9eee2a02221faa046625481449c4086f8d27e
sara altaee <sara.korea96@gmail.com>
2020-06-09 16:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution here
 function findMatching(array,string){
- 
+ return arr.filter(element => element === string.toLowerCase() || element === string.toUpperCase() || element === string.charAt(0).toUpperCase() + string.slice(1))
 }
